e1d676f7397a31a89d188a5198ba7fb6fc9924bf
edwardroger <jarvis.ejr@gmail.com>
2021-04-01 13:35:19 +0700

    change

--- day2/src/day2/Day2.java
+++ day2/src/day2/Day2.java
@@ -47,8 +47,7 @@ public class Day2 {
         int s = 0;
         for (int i = 0; i < arr.length;i++){
             for(int j = 0; j< arr[i].length;j++){
-                if(arr[i][j]==arr[j][i]){
-                } else {
+                if(i==j){
                     s+=arr[i][j];
                 }
             }
